46848b520b09cb28a15bc9fdcc1957ffd8b78ab0
anmagoS <andrumjavier@hotmail.com>
2025-10-04 22:43:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-let articulosCarrito = [];
-
 const carritoContainer = document.getElementById("carrito-contenido");
 const subtotalElement = document.getElementById("subtotal");
 const contadorCarrito = document.getElementById("contador-carrito");
