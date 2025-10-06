81f6b42122e69423f2ea09fb7d35e1c00316fec7
anmagoS <andrumjavier@hotmail.com>
2025-10-06 12:53:27 -0500

    Update app.js

--- app.js
+++ app.js
@@ -189,6 +189,11 @@ function mostrarTemporizadorPromos() {
 // === Inicialización ===
 document.addEventListener("DOMContentLoaded", async () => {
   const { tipo, subtipo, categoria } = getParametrosDesdeURL();
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("service-worker.js")
+    .then(() => console.log("✅ Service Worker registrado"))
+    .catch(err => console.error("❌ Error al registrar SW:", err));
+}
 
   await cargarCatalogoGlobal();
   mostrarTemporizadorPromos();
