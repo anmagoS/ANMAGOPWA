07324255430c09e4507dbb2421825977c439cbae
anmagoS <andrumjavier@hotmail.com>
2025-11-12 18:18:38 -0500

    Update alerta.js

--- alerta.js
+++ alerta.js
@@ -19,12 +19,8 @@
       modal = document.createElement("div");
       modal.id = "anmago-carrito";
       modal.className = "anmago-modal-carrito";
-     const referencia = document.getElementById("footer-container");
-if (referencia) {
-  referencia.insertAdjacentElement("beforebegin", modal);
-} else {
-  document.body.appendChild(modal);
-}
+    document.body.appendChild(modal);
+ 
 
 
       // Cerrar al tocar fuera del contenido
